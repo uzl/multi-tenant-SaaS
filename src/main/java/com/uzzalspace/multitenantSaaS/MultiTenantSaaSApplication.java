815e6a311dbf3be0dfeb5c3a31608871c0070dbc
Uzzal Podder <uzzwalbd@gmail.com>
2018-08-29 10:44:52 +0600

    Initial commit

---
+++ src/main/java/com/uzzalspace/multitenantSaaS/MultiTenantSaaSApplication.java
@@ -0,0 +1,12 @@
+package com.uzzalspace.multitenantSaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantSaaSApplication.class, args);
+	}
+}
